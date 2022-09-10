b743568efc81b6cb58018c9aaadd2ea549c8fec3
Hemslo Wang <hemslo.wang@gmail.com>
2022-09-10 15:18:14 +1000

    Use tfjs for edge predict

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,24 @@
-import {basename} from "path";
+import {ImageClassificationModel} from "@tensorflow/tfjs-automl";
+import {basename, join} from "path";
+import {debug, log} from "firebase-functions/logger";
 import {getFirestore} from "firebase-admin/firestore";
 import {getStorage} from "firebase-admin/storage";
 import {initializeApp} from "firebase-admin/app";
-import {log} from "firebase-functions/logger";
 import {onObjectFinalized, StorageEvent} from "firebase-functions/v2/storage";
-import {v1beta1} from "@google-cloud/automl";
+import {pathToFileURL} from "url";
+import {tmpdir} from "os";
+import {promises as fs} from "fs";
+import {loadGraphModel, node, Tensor3D} from "@tensorflow/tfjs-node";
 
 initializeApp();
+
+let imageClassificationModel: ImageClassificationModel | undefined;
 
 const waitForDoc = (doc: FirebaseFirestore.DocumentReference) =>
   new Promise<FirebaseFirestore.DocumentSnapshot<
     FirebaseFirestore.DocumentData>>(
         (r) => {
-          log("Waiting for doc to be created...");
+          debug("Waiting for doc to be created...");
           const unsubscribe = doc.onSnapshot((snapshot) => {
             if (snapshot.exists) {
               r(snapshot);
@@ -21,30 +27,46 @@ const waitForDoc = (doc: FirebaseFirestore.DocumentReference) =>
           });
         });
 
-const download = async (bucket: string, name: string) => {
-  const [image] = await getStorage().bucket(bucket).file(name!).download();
-  return image;
+const downloadModel = async (bucket: string) => {
+  const bucketRef = getStorage().bucket(bucket);
+  const [files] = await bucketRef.getFiles({prefix: process.env.MODEL_PATH});
+  await Promise.all(files.map((file) => {
+    const destination = join(tmpdir(), basename(file.name));
+    debug(`Downloading model ${destination}`);
+    return file.download({destination});
+  }));
 };
 
-const predictLabel = async (image: Buffer) => {
-  const client = new v1beta1.PredictionServiceClient();
-  const [response] = await client.predict({
-    name: client.modelPath(
-        process.env.PROJECT_ID!,
-        process.env.LOCATION!,
-        process.env.MODEL_ID!,
-    ),
-    payload: {
-      image: {
-        imageBytes: image.toString("base64"),
-      },
-    },
-  });
-  log("Prediction response", response);
-  const result = response.payload?.sort(
-      (a, b) => b.classification!.score! - a.classification!.score!,
-  )[0];
-  return result;
+const loadDictionary = async () => {
+  const text = await fs.readFile(join(tmpdir(), "dict.txt"), "utf8");
+  return text.trim().split("\n");
+};
+
+const loadImageClassification = async (bucket: string) => {
+  if (imageClassificationModel) {
+    debug("Image classification model already loaded");
+    return imageClassificationModel;
+  }
+
+  await downloadModel(bucket);
+
+  const [model, dict] = await Promise.all([
+    loadGraphModel(pathToFileURL(join(tmpdir(), "model.json")).href),
+    loadDictionary(),
+  ]);
+  debug("Loaded model and dictionary");
+
+  imageClassificationModel = new ImageClassificationModel(model, dict);
+  return imageClassificationModel;
+};
+
+const predictLabel = async (
+    image: Tensor3D,
+    model: ImageClassificationModel,
+) => {
+  const predictions = await model.classify(image);
+  debug("Predictions", predictions);
+  return predictions.sort((a, b) => b.prob - a.prob)[0];
 };
 
 const savePrediction = async (
@@ -56,12 +78,18 @@ const savePrediction = async (
   const docRef = snapshotsCollection.doc(id);
 
   const doc = await waitForDoc(docRef);
-  log("Document data", doc.data());
+  debug("Document data", doc.data());
 
   await docRef.set({
     prediction,
     score,
   }, {merge: true});
+};
+
+const prepareImage = async (bucket: string, name: string) => {
+  const [image] = await getStorage().bucket(bucket).file(name!).download();
+  debug("Image downloaded", name);
+  return node.decodeImage(Uint8Array.from(image)) as Tensor3D;
 };
 
 export const predict = onObjectFinalized(
@@ -76,10 +104,12 @@ export const predict = onObjectFinalized(
         return;
       }
 
-      const image = await download(bucket!, name!);
-      log("Image downloaded", name);
+      const [image, model] = await Promise.all([
+        prepareImage(bucket!, name!),
+        loadImageClassification(bucket!),
+      ]);
 
-      const result = await predictLabel(image);
+      const result = await predictLabel(image, model);
       if (!result) {
         return;
       }
@@ -87,8 +117,8 @@ export const predict = onObjectFinalized(
 
       await savePrediction(
           basename(name!),
-         result.displayName!,
-         result.classification!.score!,
+          result.label,
+          result.prob,
       );
       log("Prediction saved", name);
     });
