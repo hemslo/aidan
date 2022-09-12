e2c44c476cd63dc8cbccf3cef0605627a1fc8c91
Hemslo Wang <hemslo.wang@gmail.com>
2022-09-13 00:28:14 +1000

    Add Google Analytics

--- hosting/src/firebaseConfig.ts
+++ hosting/src/firebaseConfig.ts
@@ -4,5 +4,6 @@ export const firebaseConfig = {
     projectId: "aidan-is-detecting-any-needs",
     storageBucket: "aidan-is-detecting-any-needs.appspot.com",
     messagingSenderId: "522273270290",
-    appId: "1:522273270290:web:270cc75c4564c8fa3206cb"
+    appId: "1:522273270290:web:270cc75c4564c8fa3206cb",
+    measurementId: "G-BYL5444QKX"
 };
