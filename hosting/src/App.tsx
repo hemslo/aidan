046b23537507073801cb16d142aea1963930c7f9
Hemslo Wang <hemslo.wang@gmail.com>
2022-09-13 21:05:40 +1000

    Add AnalyticsProvider

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
-import {AuthProvider, FirestoreProvider, StorageProvider, useFirebaseApp} from 'reactfire';
+import {AnalyticsProvider, AuthProvider, FirestoreProvider, StorageProvider, useFirebaseApp} from 'reactfire';
 import {Authentication, AuthWrapper} from './features/authentication/Authentication';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {Navigation} from './features/navigation/Navigation';
@@ -9,6 +9,7 @@ import {Outlet} from 'react-router-dom';
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
+import {getAnalytics} from 'firebase/analytics';
 
 export const App = () => {
     const firebaseApp = useFirebaseApp();
@@ -17,23 +18,25 @@ export const App = () => {
     const storageInstance = getStorage(firebaseApp);
 
     return (
-        <AuthProvider sdk={auth}>
-            <FirestoreProvider sdk={firestoreInstance}>
-                <StorageProvider sdk={storageInstance}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <Box sx={{display: 'flex'}}>
-                            <Navigation/>
-                            <Box component="main" sx={{p: 3, width: 1}}>
-                                <Toolbar/>
-                                <Authentication/>
-                                <AuthWrapper fallback={<></>}>
-                                    <Outlet/>
-                                </AuthWrapper>
+        <AnalyticsProvider sdk={getAnalytics(firebaseApp)}>
+            <AuthProvider sdk={auth}>
+                <FirestoreProvider sdk={firestoreInstance}>
+                    <StorageProvider sdk={storageInstance}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <Box sx={{display: 'flex'}}>
+                                <Navigation/>
+                                <Box component="main" sx={{p: 3, width: 1}}>
+                                    <Toolbar/>
+                                    <Authentication/>
+                                    <AuthWrapper fallback={<></>}>
+                                        <Outlet/>
+                                    </AuthWrapper>
+                                </Box>
                             </Box>
-                        </Box>
-                    </LocalizationProvider>
-                </StorageProvider>
-            </FirestoreProvider>
-        </AuthProvider>
+                        </LocalizationProvider>
+                    </StorageProvider>
+                </FirestoreProvider>
+            </AuthProvider>
+        </AnalyticsProvider>
     );
 };
